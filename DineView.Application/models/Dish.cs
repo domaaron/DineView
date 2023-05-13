089510e42cc42d968b9dd2c92851288ceda74074
Aaron Domingo <daaronjhay@gmx.at>
2023-05-13 20:19:11 +0200

    Added additional properties

--- DineView.Application/models/Dish.cs
+++ DineView.Application/models/Dish.cs
@@ -11,20 +11,24 @@ namespace DineView.Application.models
     [Table("Dish")]
     public class Dish
     {
-        public Dish(string name, string description)
+        public Dish(string name, string description, float calories, bool isSpicy)
         {
             Name = name;
             Description = description;
+            Calories = calories;
+            IsSpicy = isSpicy;
         }
-
+        
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         protected Dish() { }
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; private set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public float Calories { get; set; }
+        public bool IsSpicy { get; set; }
+        public virtual ICollection<Menu> Menus { get; } = new List<Menu>();
 
     }
 }
