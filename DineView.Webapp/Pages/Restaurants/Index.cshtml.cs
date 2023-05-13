d4579f83d1b35aa5a0bf4a5ac064eb8df9879137
Aaron Domingo <daaronjhay@gmx.at>
2023-05-13 20:21:10 +0200

    Added record DinesWithMenusCount()

--- DineView.Webapp/Pages/Restaurants/Index.cshtml.cs
+++ DineView.Webapp/Pages/Restaurants/Index.cshtml.cs
@@ -2,6 +2,8 @@ using DineView.Application.infrastructure;
 using DineView.Application.models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -9,8 +11,23 @@ namespace DineView.Webapp.Pages.Restaurants
 {
     public class IndexModel : PageModel
     {
+        public record DinesWithMenusCount(
+            Guid Guid,
+            string Name,
+            int menusCount,
+            string Street,
+            string District,
+            TimeOnly OpeningTime,
+            TimeOnly ClosedTime,
+            string CuisineStyle,
+            bool IsOrderable,
+            string Rating,
+            string Tel,
+            string URL
+            );
+
         private readonly DineContext _db;
-        public List<Restaurant> Restaurants { get; private set; } = new();
+        public List<DinesWithMenusCount> Restaurants { get; private set; } = new();
         public IndexModel(DineContext db)
         {
             _db = db;
@@ -18,7 +35,23 @@ namespace DineView.Webapp.Pages.Restaurants
 
         public void OnGet()
         {
-            Restaurants = _db.Restaurants.OrderBy(r => r.Id).ToList();
+            Restaurants = _db.Restaurants
+                .OrderBy(r => r.Name)
+                .Select(r => new DinesWithMenusCount(
+                r.Guid,
+                r.Name,
+                r.Menus.Count(),
+                r.Address.Street,
+                r.Address.District,
+                r.OpeningTime,
+                r.ClosedTime,
+                r.CuisineStyle,
+                r.IsOrderable,
+                r.Rating,
+                r.Tel,
+                r.URL
+                ))
+                .ToList();
         }
     }
 }
