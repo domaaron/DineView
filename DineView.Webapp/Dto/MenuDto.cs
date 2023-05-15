ba2507b9397b19cfb0dc365851cf54244f6e26e1
Aaron Domingo <daaronjhay@gmx.at>
2023-05-15 11:21:05 +0200

    Created

---
+++ DineView.Webapp/Dto/MenuDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DineView.Webapp.Dto
+{
+    public record MenuDto
+    (
+        Guid guid,
+        [Range(2, 500, ErrorMessage = "Invalid price")]
+        decimal Price,
+        string Name,
+        string Description,
+        float Calories,
+        bool IsSpicy,
+        Guid DishGuid,
+        Guid RestaurantGuid
+    );
+}
